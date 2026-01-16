dabc078085b37f3d456c082fdcce91d090ea629a
Abdul Rehman <abdulrehmanshafy101@gmail.com>
2026-01-16 18:29:20 +0500

    update the coed in core app view

--- trackforge/core/views.py
+++ trackforge/core/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
 from inventory.models import Product, Warehouse, Stock, Category
+from accounts.models import CustomUser
+from procurement.models import Supplier, PurchaseOrder # Import these when ready
 
-# from procurement.models import Supplier, PurchaseOrder # Import these when ready
 
-User = get_user_model()
 
 
 def dashboard(request):
@@ -16,11 +16,11 @@ def dashboard(request):
         'warehouse_count': Warehouse.objects.count(),
         'stock_count': Stock.objects.count(),
         'category_count': Category.objects.count(),
-        'user_count': User.objects.count(),
+        'user_count': CustomUser.objects.count(),
+        'supplier_count': Supplier.objects.count(),
 
         # If you haven't made the procurement models yet,
         # keep these commented out or set to 0 to avoid errors.
-        # 'supplier_count': Supplier.objects.count(),
         # 'po_count': PurchaseOrder.objects.count(),
     }
     return render(request, 'dashboard.html', context)
