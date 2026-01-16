d1b61506190e22ec88c3064ac257862fe53e6e2d
Abdul Rehman <abdulrehmanshafy101@gmail.com>
2026-01-16 18:30:53 +0500

    added the PoLineItemForm and inline formset to join 2 forms

--- trackforge/procurement/forms.py
+++ trackforge/procurement/forms.py
@@ -1,14 +1,27 @@
 from django import forms
-from .models import PurchaseOrder,Supplier
+from .models import *
+from django.forms import inlineformset_factory
 
 class SupplierForm(forms.ModelForm):
     class Meta:
         model = Supplier
-        fields = '__all__'
-
+        fields = ["name",'email',"phone",'address']
 
 class PurchaseOrderForm(forms.ModelForm):
     class Meta:
         model = PurchaseOrder
-        fields = ["supplier","status"]
+        fields = ["reference_number","supplier","warehouse"]
 
+class POLineItemForm(forms.ModelForm):
+    class Meta:
+        model = POLineItem
+        fields = ["product","quantity","unit_price"]
+
+# In your forms.py
+POLineItemFormSet = inlineformset_factory(
+    PurchaseOrder,
+    POLineItem,
+    form=POLineItemForm,
+    extra=3,
+    can_delete=True,
+)
